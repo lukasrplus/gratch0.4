dd40969dda434525032c8b8aeead5a9a4660382b
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-08-14 13:36:04 +0200

    JS works for all different gratches - moves right boxes - basis for indiv forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,39 +16,59 @@
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-  // $('#click_gratch_details').click(function(e){
-  //   e.preventDefault();
-  //   $('.specific').toggle('slow');
-  // });
+// $(function() {
+//   // $('#click_gratch_details').click(function(e){
+//   //   e.preventDefault();
+//   //   $('.specific').toggle('slow');
+//   // });
 
 
-  // $('#click_gratch_friends').click(function(e){
-  //   e.preventDefault();
-  //   $('.friends').toggle('slow');
-  // });
+//   // $('#click_gratch_friends').click(function(e){
+//   //   e.preventDefault();
+//   //   $('.friends').toggle('slow');
+//   // });
 
-  // START OF INDIVIDUAL TOGGLES ON GRATCH LIST
+//   // START OF INDIVIDUAL TOGGLES ON GRATCH LIST
 
-  var gratch_id = $('.click_list').attr('id');
-  console.log(gratch_id);
+//   // var gratch_id = $('.click_list').attr('id');
+//   // console.log(gratch_id);
 
-  var number_of_gratches = $('.click_list').size(function(){
-    return this.lenght;
+//   var number_of_gratches = $('.click_list').size(function(){
+//     return this.lenght;
+//   });
+//   console.log(number_of_gratches);
+
+//   $('#click_gratch_friends').click(function(e){
+//     e.preventDefault();
+//     console.log(this.attr('data-friends'));
+
+//   });
+
+//   var test = $('#click_gratch_friends').attr('data-friends');
+
+//   console.log(test);
+
+// $('.details_click').on('click', function(){
+//   var gratch_id = this.data('click_details');
+//   console.log(gratch_id)
+// })
+
+
+
+
+//   // END OF INDIVIDUAL TOGGLES ON GRATCH LIST
+
+// });
+
+
+$(function(){
+
+  $('.details_click').click(function(){
+    var item = $(this);
+    var gratch_id = item.data('click-details');
+
+    $('#specific_id_' + gratch_id).toggle('slow');
+
   });
-  console.log(number_of_gratches);
-
-  $('#click_gratch_friends').click(function(e){
-    e.preventDefault();
-    console.log(this.attr('data-friends'));
-
-  });
-
-  var test = $('#click_gratch_friends').attr('data-friends');
-
-  console.log(test);
-
-
-  // END OF INDIVIDUAL TOGGLES ON GRATCH LIST
 
 });
