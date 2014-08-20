e028c764ab22803bb27acdd77d3bd51a0cfc7916
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-08-20 16:48:07 +0200

    Some more advances on the apply bid form

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,19 +1,17 @@
 class BidsController < ApplicationController
 
   def new
-    # if current_user.present?
-    #   @gratch = Gratch.find_by(:id => params[:gratch_id])
-    #   render new
-    # else
-    #   redirect_to root_url
-    # end
-    @bid = Bid.new
+      @gratch = Gratch.find_by(:id => params[:gratch_id])
+      @host = User.find_by(:id => @gratch.host_id)
+      @bid = Bid.new
   end
 
   def create
    if params.has_key?(:bid)
     params.require(:bid).permit(:applicant_id => [])
     hash = params
+    bid = Bid.new
+
     # HAS TO BE ADOPTED
     redirect_to root_url
     # HAS TO BE ADOPTED
